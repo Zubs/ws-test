d37436d0143614be00d1e0ce484b4a44bfadb22c
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2024-04-28 19:56:53 +0100

    feat: init

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server({
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.json({ name: "Zubs" });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
